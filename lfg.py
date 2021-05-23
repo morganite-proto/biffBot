b53b197aee1b92393848a6720dc3e1f8ffebc94f
Morganite <morganiteproto@protonmail.com>
2021-05-23 12:11:59 -0700

    Made time with the LFG role a variable

--- lfg.py
+++ lfg.py
@@ -25,21 +25,21 @@ class LFGCog(commands.Cog):
         # the LFG DB
 
         # MySQL datetime is YYYY-MM-DD hh:mm:ss
-        if discord.utils.get(ctx.author.roles, id=cfg.roleLFG) is None:
+        if discord.utils.get(ctx.author.roles, id=cfg.lfgRole) is None:
             now = datetime.datetime.now()
-            end = now + datetime.timedelta(hours=.5)
+            end = now + datetime.timedelta(hours=cfg.lfgTime)
             sql = ("INSERT INTO lfg VALUES (" + str(ctx.author.id) + ", '" + now.strftime(
                 "%G-%m-%d %H:%M:%S") + "', '" + end.strftime("%G-%m-%d %H:%M:%S") + "')")
             cursor.execute(sql)
             db.commit()
-            await ctx.author.add_roles(discord.Object(cfg.roleLFG))
+            await ctx.author.add_roles(discord.Object(cfg.lfgRole))
             await ctx.send(":information_source: Gave you the LFG role!")
 
         else:
             sql = ("DELETE FROM lfg WHERE lfg_id = " + str(ctx.author.id))
             cursor.execute(sql)
             db.commit()
-            await ctx.author.remove_roles(discord.Object(cfg.roleLFG))
+            await ctx.author.remove_roles(discord.Object(cfg.lfgRole))
             await ctx.send(":information_source: Removed your LFG role!")
 
 
